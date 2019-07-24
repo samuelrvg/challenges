e5a43494d251a91818ccbf71eace94649bf09cd7
Samuel Vaz <samuel.rvg@gmail.com>
2019-07-24 00:38:09 -0300

    challenge 04

--- Challenge-04/Program.cs
+++ Challenge-04/Program.cs
@@ -13,38 +13,47 @@ class Result
      * The function accepts 2D_INTEGER_ARRAY arr as parameter.
      */
 
-    public static int diagonalDifference(List<List<int>> arr)
+    public static int diagonalDifference(int[,] arr)
     {
-        var array = new int[3, 3];
-        array[0, 0] = 5;
-        array[0, 1] = 8;
-        array[0, 2] = 10;
+        //var array = new int[3, 3];
+        //array[0, 0] = 5;
+        //array[0, 1] = 8;
+        //array[0, 2] = 10;
 
-        array[1, 0] = 9;
-        array[1, 1] = 6;
-        array[1, 2] = 7;
+        //array[1, 0] = 9;
+        //array[1, 1] = 6;
+        //array[1, 2] = 7;
 
-        array[2, 0] = 2;
-        array[2, 1] = 3;
-        array[2, 2] = 1;
+        //array[2, 0] = 2;
+        //array[2, 1] = 3;
+        //array[2, 2] = 1;
 
-        //int rank = array.Rank;
-        var rank = (int)Math.Sqrt(arr.Count - 1);
-        int diagonalDireitaParaEsquerda = 0;
-        int diagonalEsquerdaParaDireita = 0;
+        int rank = arr.Rank;
+        int diagonalPrimaria = 0;
+        int diagonalSecundaria = 0;
         for (int i = 0; i <= rank; i++)
         {
-            if (array[i, i] == array[i, i])
+            if (arr[i, i] == arr[i, i])
             {
-                diagonalDireitaParaEsquerda += array[i, i];
+                diagonalPrimaria += arr[i, i];
             }
-            if (array[i, rank - i] == array[i, rank - i])
+            if (arr[i, rank - i] == arr[i, rank - i])
             {
-                diagonalEsquerdaParaDireita += array[i, rank - i];
+                diagonalSecundaria += arr[i, rank - i];
             }
         }
 
-        return diagonalDireitaParaEsquerda - diagonalEsquerdaParaDireita;
+        int result = 0;
+        if(diagonalPrimaria > diagonalSecundaria)
+        {
+            result = diagonalPrimaria - diagonalSecundaria;
+        }
+        else
+        {
+            result = diagonalSecundaria - diagonalPrimaria;
+        }
+
+        return result;
     }
 
 }
@@ -57,18 +66,35 @@ namespace Challenge_04
         {
             //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-            //int n = Convert.ToInt32(Console.ReadLine().Trim());
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-            //List<List<int>> arr = new List<List<int>>();
+            List<List<int>> arr = new List<List<int>>();
+            int[,] arr2 = new int[n,n];
 
-            //for (int i = 0; i < n; i++)
-            //{
-            //    arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
-            //}
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
 
-            int result = Result.diagonalDifference(null);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i][j] <= 100 && arr[i][j] >= -100)
+                    {
+                        arr2[i, j] = arr[i][j];
+                    }
+                    else
+                    {
+                        arr[i][j] = 99;
+                    }
+                }
+            }
+
+            int result = Result.diagonalDifference(arr2);
 
             Console.WriteLine(result);
+            Console.ReadKey();
 
             //textWriter.WriteLine(result);
 
